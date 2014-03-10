3a58dc66b4923ebff55ec0b79974cbf257399693
lightuono <lightuono@toshidex>
2014-03-10 23:11:24 +0100

    * Aggiunta funziona HELP. Controlla il passaggio di parametri * Aggiunta funzione KILL.

--- fuck.sh
+++ fuck.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+V 0.2
 
 
+declare -a opt
+opt=($@)
+numopt=$#
 
 
 function FUCK(){
 
-cat <<FUCKYOU
+echo
+echo
+echo -e "\t\t $i"
 
+cat <<FUCKYOU
 
 	_                         _
        |_|                       |_|
@@ -24,5 +31,48 @@ cat <<FUCKYOU
 		  ooO Ooo
 FUCKYOU
 
+echo
+echo
 }
+
+
+function HELP() {
+
+cat <<EOF
+
+Uso: fuck you [PROGRAMMA] or [OPZIONE]
+
+EOF
+
+}
+
+
+function KILL(){
+	
+	j=1
+	
+	for i in ${opt[@]:1}; do
+		if [[ $i != "" ]]; then
+	#		echo "Sto uccidendo... $i"
+			FUCK $i
+		fi
+		j=$((++j))
+	done
+}
+	
+	
+
+
+
+if [[ $numopt == 0 ]]; then
+	HELP
+else
+	if [[ $numopt -ge 1 && ${opt[0]} != "you" ]]; then
+		HELP
+	
+	else
+		KILL
+	fi
+fi
+
 
